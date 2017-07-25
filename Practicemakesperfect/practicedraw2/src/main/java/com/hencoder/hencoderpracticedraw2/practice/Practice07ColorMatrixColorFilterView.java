69cd038b42029c6a6927745373fc569626468418
lipin <1079107009@qq.com>
2017-07-25 13:24:26 +0800

    学习paint绘制

---
+++ Practicemakesperfect/practicedraw2/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice07ColorMatrixColorFilterView.java
@@ -0,0 +1,48 @@
+package com.hencoder.hencoderpracticedraw2.practice;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
+
+public class Practice07ColorMatrixColorFilterView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
+
+    public Practice07ColorMatrixColorFilterView(Context context) {
+        super(context);
+    }
+
+    public Practice07ColorMatrixColorFilterView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice07ColorMatrixColorFilterView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+
+        // 使用 setColorFilter() 设置一个 ColorMatrixColorFilter
+        ColorMatrix matrix = new ColorMatrix();
+        // 用 ColorMatrixColorFilter.setSaturation() 把饱和度去掉
+        matrix.setSaturation(0);
+        paint.setColorFilter(new ColorMatrixColorFilter(matrix));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+    }
+}
---
+++ Practicemakesperfect/practicedraw2/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
@@ -0,0 +1,97 @@
+package com.hencoder.hencoderpracticedraw2.practice;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.SumPathEffect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Practice12PathEffectView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Path path = new Path();
+
+    public Practice12PathEffectView(Context context) {
+        super(context);
+    }
+
+    public Practice12PathEffectView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice12PathEffectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        paint.setStyle(Paint.Style.STROKE);
+
+        path.moveTo(50, 100);
+        path.rLineTo(50, 100);
+        path.rLineTo(80, -150);
+        path.rLineTo(100, 100);
+        path.rLineTo(70, -120);
+        path.rLineTo(150, 80);
+    }
+
+    @SuppressLint("DrawAllocation")
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // 使用 Paint.setPathEffect() 来设置不同的 PathEffect
+
+        // 第一处：CornerPathEffect
+        paint.setPathEffect(new CornerPathEffect(5));
+        canvas.drawPath(path, paint);
+
+        canvas.save();
+        canvas.translate(500, 0);
+        // 第二处：DiscretePathEffect
+        DiscretePathEffect discretePathEffect = new DiscretePathEffect(20, 5);
+        paint.setPathEffect(discretePathEffect);
+        canvas.drawPath(path, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.translate(0, 200);
+        // 第三处：DashPathEffect
+        DashPathEffect dashPathEffect = new DashPathEffect(new float[]{10, 5}, 10);
+        paint.setPathEffect(dashPathEffect);
+        canvas.drawPath(path, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.translate(500, 200);
+        // 第四处：PathDashPathEffect
+        Path dashPath = new Path();
+        dashPath.lineTo(20, -30);
+        dashPath.lineTo(40, 0);
+        dashPath.close();
+        paint.setPathEffect(new PathDashPathEffect(dashPath, 50, 0, PathDashPathEffect.Style.MORPH));
+        canvas.drawPath(path, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.translate(0, 400);
+        // 第五处：SumPathEffect
+        paint.setPathEffect(new SumPathEffect(dashPathEffect, discretePathEffect));
+        canvas.drawPath(path, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.translate(500, 400);
+        // 第六处：ComposePathEffect
+        paint.setPathEffect(new ComposePathEffect(dashPathEffect, discretePathEffect));
+        canvas.drawPath(path, paint);
+        canvas.restore();
+    }
+}
