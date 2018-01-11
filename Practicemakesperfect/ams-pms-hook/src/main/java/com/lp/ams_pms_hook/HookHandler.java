e5f3f15cff23321ee6085a3cde2997fd463cc11c
lp <1079107009@qq.com>
2018-01-11 10:37:43 +0800

    理解动态代理

--- Practicemakesperfect/ams-pms-hook/src/main/java/com/lp/ams_pms_hook/HookHandler.java
+++ Practicemakesperfect/ams-pms-hook/src/main/java/com/lp/ams_pms_hook/HookHandler.java
@@ -20,6 +20,13 @@ class HookHandler implements InvocationHandler {
         mBase = base;
     }
 
+    /**
+     * @param proxy  指代我们所代理的那个真实对象
+     * @param method 指代的是我们所要调用真实对象的某个方法的Method对象
+     * @param args   指代的是调用真实对象某个方法时接受的参数
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Log.d(TAG, "hey, baby; you are hooked!!");
