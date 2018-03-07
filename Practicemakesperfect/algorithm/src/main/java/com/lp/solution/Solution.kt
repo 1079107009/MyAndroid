6be19222c6badd7d9c89e4e310a3d1519106f1b4
lp <1079107009@qq.com>
2018-03-07 11:15:45 +0800

    算法四题

--- Practicemakesperfect/algorithm/src/main/java/com/lp/solution/Solution.kt
+++ Practicemakesperfect/algorithm/src/main/java/com/lp/solution/Solution.kt
@@ -43,3 +43,67 @@ fun islandPerimeter(grid: Array<IntArray>): Int {
 
     return islands * 4 - neighbours * 2
 }
+
+/**
+ * 657. Judge Route Circle
+ */
+fun judgeCircle(moves: String): Boolean {
+    var v = 0
+    var h = 0
+    moves.toCharArray().forEach {
+        when (it) {
+            'U' -> v++
+            'D' -> v--
+            'L' -> h--
+            'R' -> h++
+        }
+    }
+    return v == 0 && h == 0
+}
+
+/**
+ * 500. Keyboard Row
+ */
+fun findWords(words: Array<String>): Array<String> {
+    return words.filter {
+        it.toLowerCase().matches(Regex("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*"))
+    }.toTypedArray()
+}
+
+/**
+ * 766. Toeplitz Matrix
+ */
+fun isToeplitzMatrix(matrix: Array<IntArray>): Boolean {
+    var res = 0
+    val h = matrix.size - 1
+    val w = matrix[0].size - 1
+    for (i in matrix.size - 1 downTo 1) {
+        (matrix[i].size - 1 downTo 1)
+                .filter { matrix[i][it] == matrix[i - 1][it - 1] }
+                .forEach { res++ }
+    }
+    return res == w * h
+}
+
+/**
+ * 496. Next Greater Element I
+ */
+fun nextGreaterElement(nums1: IntArray, nums2: IntArray): IntArray {
+    val res = IntArray(nums1.size)
+    for (i in nums1.indices) {
+        nums2.sliceArray(i + 1 until nums2.size).forEach {
+            if (it > nums1[i]) {
+                res[i] = it
+            } else {
+                res[i] = -1
+            }
+        }
+    }
+    return res
+}
+
+fun main(args: Array<String>) {
+    val nums1 = arrayOf(4, 1, 2)
+    val nums2 = arrayOf(1, 3, 4, 2)
+    nextGreaterElement(nums1.toIntArray(), nums2.toIntArray())
+}
